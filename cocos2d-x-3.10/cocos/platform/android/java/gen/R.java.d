35a3a40e92a03b0c1e4da7cc0139098450d72674
YuukiTanihara <wide.tani@gmail.com>
2016-06-19 05:13:02 +0900

    cocos2d-x-3.10 delete in folder

--- cocos2d-x-3.10/cocos/platform/android/java/gen/R.java.d
+++
@@ -1,1 +0,0 @@
- : /Users/taniharayuuki/Desktop/work/CrowdWorks/crypt/cocos2d-x-3.10/cocos/platform/android/java/bin/AndroidManifest.xml \
--- cocos2d-x-3.10/cocos/platform/android/java/gen/com/enhance/gameservice/IGameTuningService.java.d
+++
@@ -1,3 +0,0 @@
-/Users/taniharayuuki/Desktop/work/CrowdWorks/crypt/cocos2d-x-3.10/cocos/platform/android/java/gen/com/enhance/gameservice/IGameTuningService.java: \
-  /Users/taniharayuuki/Desktop/work/CrowdWorks/crypt/cocos2d-x-3.10/cocos/platform/android/java/src/com/enhance/gameservice/IGameTuningService.aidl 
-
